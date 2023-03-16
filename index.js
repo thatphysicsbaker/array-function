f3dd234db6fe84d1255a575c2e4525fe82b312a1
Abigail <abieniye099@gmail.com>
2023-03-16 10:14:57 +0100

    First comment

---
+++ index.js
@@ -0,0 +1,44 @@
+// What are the differences between mutating array methods and non-mutation array methods in JavaScript. List 5 array methods that fall under each of them.
+// Ans
+// Mutation is the act of changing the properties of a value WHILE Non mutation method do not change the object after the method has been used.
+
+//Example of mutating method
+//Array.push
+//Array.pop
+//Array.unshift
+//Array.fill
+//splice method
+
+//Example of non mutating method
+//Array.concat
+//Array.filter
+//Array.map
+
+// Question 2
+// 1. let array = ['C#', 'JavaScript', 'Ruby', 'PHP', 'Python']
+//array.push('Kotlin')
+//console.log(array)
+
+// 2. let array = ['C#', 'JavaScript', 'Ruby', 'PHP', 'Python']
+// array.splice(3, 0, 'Java')
+//console.log(array)
+
+// 3. let array = ['C#', 'JavaScript', 'Ruby', 'PHP', 'Python']
+//array.shift('c#')
+//console.log(array)
+
+// 4. let array = ['C#', 'JavaScript', 'Ruby', 'PHP', 'Python']
+//array.unshift('Scala', 'Swift')
+//console.log(array)
+
+// 5. let array = ['C#', 'JavaScript', 'Ruby', 'PHP', 'Python']
+//array.splice(3, 1, 'Go', 'Rust')
+//console.log(array)
+
+
+3. //let fruit = ['apple', 'mango', 'banana'];
+   //function changeFruit (fruit) {
+  //fruit [2] = "orange";
+  //return fruit;
+//}
+//console.log(changeFruit(fruit))
